3d39b42e2cba0ede7393df10f260d2d53c13af94
Herman0802 <germanlazar02@gmail.com>
2023-02-12 22:13:36 +0100

    Filled tables

--- SQLQuery_HL.sql
+++ SQLQuery_HL.sql
@@ -1,37 +1,37 @@
 /*
 Entity–relationship model:
 
-users
+USERS
   |
-  v
-games  <-  game_developers
+  V
+GAMES  <-  GAME_DEVELOPERS
   |
-  v
-developers
+  V
+DEVELOPERS
   |
-  v
-publishers  <-  game_publishers
+  V
+PUBLISHERS  <-  GAME_PUBLISHERS
   |
-  v
-platforms
+  V
+PLATFORMS
   |
-  v
-reviews  <-  ratings
+  V
+REVIEWS  <-  RATINGS
   |
-  v
-wishlist
+  V
+WISHLIST
   |
-  v
-cart
+  V
+CART
   |
-  v
-orders  <-  order_items  <-  shipping_address  <-  payment_methods
+  V
+ORDERS  <-  ORDER_ITEMS  <-  SHIPPING_ADDRESS  <-  PAYMENT_METHODS
   |
-  v
-game_tags
+  V
+GAME_TAGS
   |
-  v
-game_awards
+  V
+GAME_AWARDS
 */
 
 CREATE TABLE USERS (
@@ -40,6 +40,14 @@ CREATE TABLE USERS (
   EMAIL VARCHAR(255) NOT NULL UNIQUE,
   PASSWORD VARCHAR(255) NOT NULL
 );
+
+INSERT INTO USERS (ID, USERNAME, EMAIL, PASSWORD)
+VALUES
+  (1, 'john_doe', 'john_doe@example.com', 'password1'),
+  (2, 'jane_doe', 'jane_doe@example.com', 'password2'),
+  (3, 'jim_smith', 'jim_smith@example.com', 'password3'),
+  (4, 'sara_lee', 'sara_lee@example.com', 'password4'),
+  (5, 'tom_jones', 'tom_jones@example.com', 'password5');
 
 CREATE TABLE GAMES (
   ID INT PRIMARY KEY,
@@ -50,12 +58,28 @@ CREATE TABLE GAMES (
   PRICE DECIMAL(10,2) NOT NULL
 );
 
+INSERT INTO GAMES (ID, TITLE, GENRE, RELEASE_DATE, DESCRIPTION, PRICE)
+VALUES
+  (1, 'The Last of Us Part II', 'Action-adventure', '2020-06-19', 'A post-apocalyptic action-adventure game developed by Naughty Dog', 59.99),
+  (2, 'Red Dead Redemption 2', 'Action-adventure', '2018-10-26', 'An action-adventure game developed by Rockstar Studios', 59.99),
+  (3, 'God of War', 'Action-adventure', '2018-04-20', 'A soft reboot of the God of War series developed by Santa Monica Studio', 39.99),
+  (4, 'Halo 5: Guardians', 'First-person shooter', '2015-10-27', 'A first-person shooter developed by 343 Industries', 59.99),
+  (5, 'Minecraft', 'Sandbox', '2011-11-18', 'A sandbox game developed by Mojang Studios', 26.95);
+
 CREATE TABLE DEVELOPERS (
   ID INT PRIMARY KEY,
   NAME VARCHAR(255) NOT NULL,
   DESCRIPTION TEXT,
   WEBSITE VARCHAR(255)
 );
+
+INSERT INTO DEVELOPERS (ID, NAME, DESCRIPTION, WEBSITE)
+VALUES
+  (1, 'Naughty Dog', 'A first-party video game developer based in Santa Monica, California', 'naughtydog.com'),
+  (2, 'Rockstar Studios', 'A subsidiary of Rockstar Games based in Edinburgh, Scotland', 'rockstargames.com'),
+  (3, 'Santa Monica Studio', 'A first-party video game developer based in Santa Monica, California', 'sms.playstation.com'),
+  (4, '343 Industries', 'An American video game development studio located in Redmond, Washington', '343industries.com'),
+  (5, 'Mojang Studios', 'A video game development studio based in Stockholm, Sweden', 'mojang.com');
 
 CREATE TABLE PUBLISHERS (
   ID INT PRIMARY KEY,
@@ -64,10 +88,27 @@ CREATE TABLE PUBLISHERS (
   WEBSITE VARCHAR(255)
 );
 
+INSERT INTO PUBLISHERS (ID, NAME, DESCRIPTION, WEBSITE) 
+VALUES 
+(1, 'Electronic Arts', 'Electronic Arts (EA) is a leading publisher and developer of interactive entertainment and video games.', 'ea.com'),
+(2, 'Activision Blizzard', 'Activision Blizzard is a leading publisher of interactive entertainment and video games.', 'activisionblizzard.com'),
+(3, 'Ubisoft', 'Ubisoft is a leading publisher and developer of video games and interactive entertainment.', 'ubisoft.com'),
+(4, 'Take-Two Interactive', 'Take-Two Interactive is a leading publisher of interactive entertainment and video games.', 'take2games.com'),
+(5, 'Microsoft', 'Microsoft is a leading technology company that is also involved in the publishing of video games and interactive entertainment.', 'microsoft.com');
+
+
 CREATE TABLE PLATFORMS (
   ID INT PRIMARY KEY,
   NAME VARCHAR(255) NOT NULL
 );
+
+INSERT INTO PLATFORMS (ID, NAME) 
+VALUES 
+(1, 'PlayStation 4'),
+(2, 'Xbox One'),
+(3, 'Nintendo Switch'),
+(4, 'PC'),
+(5, 'Mobile');
 
 CREATE TABLE REVIEWS (
   ID INT PRIMARY KEY,
@@ -78,6 +119,14 @@ CREATE TABLE REVIEWS (
   FOREIGN KEY (GAME_ID) REFERENCES GAMES (ID)
 );
 
+INSERT INTO REVIEWS (ID, USER_ID, GAME_ID, REVIEW) 
+VALUES 
+(1, 1, 1, 'Great game with fantastic graphics and gameplay!'),
+(2, 2, 1, 'Loved the storyline and the character development.'),
+(3, 3, 2, 'Not the best game I have played, but it is still fun.'),
+(4, 4, 3, 'The graphics are impressive, but the gameplay is a bit repetitive.'),
+(5, 5, 4, 'I would recommend this game to anyone looking for a challenging experience.');
+
 CREATE TABLE RATINGS (
   ID INT PRIMARY KEY,
   USER_ID INT NOT NULL,
@@ -87,6 +136,14 @@ CREATE TABLE RATINGS (
   FOREIGN KEY (GAME_ID) REFERENCES GAMES (ID)
 );
 
+INSERT INTO RATINGS (ID, USER_ID, GAME_ID, RATING) 
+VALUES 
+(1, 1, 1, 9),
+(2, 2, 1, 8),
+(3, 3, 2, 7),
+(4, 4, 3, 6),
+(5, 5, 4, 9);
+
 CREATE TABLE WISHLIST (
   ID INT PRIMARY KEY,
   USER_ID INT NOT NULL,
@@ -94,6 +151,14 @@ CREATE TABLE WISHLIST (
   FOREIGN KEY (USER_ID) REFERENCES USERS (ID),
   FOREIGN KEY (GAME_ID) REFERENCES GAMES (ID)
 );
+
+INSERT INTO WISHLIST (ID, USER_ID, GAME_ID) 
+VALUES 
+(1, 1, 2),
+(2, 2, 3),
+(3, 3, 4),
+(4, 4, 5),
+(5, 5, 1);
 
 CREATE TABLE CART (
   ID INT PRIMARY KEY,
@@ -104,6 +169,14 @@ CREATE TABLE CART (
   FOREIGN KEY (GAME_ID) REFERENCES GAMES (ID)
 );
 
+INSERT INTO CART (ID, USER_ID, GAME_ID, QUANTITY) 
+VALUES 
+(1, 1, 2, 1),
+(2, 2, 3, 2),
+(3, 3, 4, 3),
+(4, 4, 5, 4),
+(5, 5, 1, 5);
+
 CREATE TABLE ORDERS (
   ID INT PRIMARY KEY,
   USER_ID INT NOT NULL,
@@ -111,6 +184,14 @@ CREATE TABLE ORDERS (
   TOTAL_AMOUNT DECIMAL(10,2) NOT NULL,
   FOREIGN KEY (USER_ID) REFERENCES USERS (ID)
 );
+
+INSERT INTO ORDERS (ID, USER_ID, ORDER_DATE, TOTAL_AMOUNT) 
+VALUES 
+(1, 1, '2022-12-01', 200.00),
+(2, 2, '2022-11-15', 150.00),
+(3, 3, '2022-10-31', 100.00),
+(4, 4, '2022-09-15', 75.00),
+(5, 5, '2022-08-01', 50.00);
 
 CREATE TABLE ORDER_ITEMS (
   ID INT PRIMARY KEY,
@@ -122,6 +203,14 @@ CREATE TABLE ORDER_ITEMS (
   FOREIGN KEY (GAME_ID) REFERENCES GAMES (ID)
 );
 
+INSERT INTO ORDER_ITEMS (ID, ORDER_ID, GAME_ID, QUANTITY, PRICE) 
+VALUES 
+  (1, 1, 1, 2, 59.99),
+  (2, 2, 2, 1, 49.99),
+  (3, 3, 3, 3, 39.99),
+  (4, 4, 4, 4, 29.99),
+  (5, 5, 5, 5, 19.99);
+
 CREATE TABLE GAME_PLATFORMS (
   ID INT PRIMARY KEY,
   GAME_ID INT NOT NULL,
@@ -129,6 +218,14 @@ CREATE TABLE GAME_PLATFORMS (
   FOREIGN KEY (GAME_ID) REFERENCES GAMES (ID),
   FOREIGN KEY (PLATFORM_ID) REFERENCES PLATFORMS (ID)
 );
+
+INSERT INTO GAME_PLATFORMS (ID, GAME_ID, PLATFORM_ID) 
+VALUES 
+  (1, 1, 1),
+  (2, 2, 2),
+  (3, 3, 3),
+  (4, 4, 4),
+  (5, 5, 5);
 
 CREATE TABLE GAME_DEVELOPERS (
   ID INT PRIMARY KEY,
@@ -138,6 +235,14 @@ CREATE TABLE GAME_DEVELOPERS (
   FOREIGN KEY (DEVELOPER_ID) REFERENCES DEVELOPERS (ID)
 );
 
+INSERT INTO GAME_DEVELOPERS (ID, GAME_ID, DEVELOPER_ID) 
+VALUES 
+  (1, 1, 1),
+  (2, 2, 2),
+  (3, 3, 3),
+  (4, 4, 4),
+  (5, 5, 5);
+
 CREATE TABLE GAME_PUBLISHERS (
   ID INT PRIMARY KEY,
   GAME_ID INT NOT NULL,
@@ -146,6 +251,14 @@ CREATE TABLE GAME_PUBLISHERS (
   FOREIGN KEY (PUBLISHER_ID) REFERENCES PUBLISHERS (ID)
 );
 
+INSERT INTO GAME_PUBLISHERS (ID, GAME_ID, PUBLISHER_ID) 
+VALUES 
+  (1, 1, 1),
+  (2, 2, 2),
+  (3, 3, 3),
+  (4, 4, 4),
+  (5, 5, 5);
+
 CREATE TABLE GAME_AWARDS (
   ID INT PRIMARY KEY,
   GAME_ID INT NOT NULL,
@@ -153,3 +266,11 @@ CREATE TABLE GAME_AWARDS (
   YEAR INT NOT NULL,
   FOREIGN KEY (GAME_ID) REFERENCES GAMES (ID)
 );
+
+INSERT INTO GAME_AWARDS (ID, GAME_ID, AWARD_NAME, YEAR) 
+VALUES 
+  (1, 1, 'Best Game of the Year', 2022),
+  (2, 2, 'Best RPG of the Year', 2022),
+  (3, 3, 'Best Sports Game of the Year', 2022),
+  (4, 4, 'Best Adventure Game of the Year', 2022),
+  (5, 5, 'Best Multiplayer Game of the Year', 2022);
