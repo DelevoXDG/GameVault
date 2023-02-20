e32edcd37dd253263949f186f866ee86b6d28937
DelevoXDG <max.on777c@gmail.com>
2023-02-20 02:11:40 +0100

    added help, changes in assets structure

--- src/app.py
+++ src/app.py
@@ -1,11 +1,17 @@
-from PyQt6.QtSql import *
-from PyQt6.QtWidgets import *
-from PyQt6.QtGui import *
-from PyQt6.QtCore import Qt, QDate
+
+from PyQt6.QtSql import QSqlRelationalTableModel, QSqlDatabase, QSqlQuery
+from PyQt6.QtWidgets import QApplication, QProxyStyle, QTableView, QAbstractItemView, QHeaderView, QVBoxLayout, QWidget, QHBoxLayout, QPushButton, QStyle, QDialog, QLabel, QDialogButtonBox, QAbstractButton
+from PyQt6.QtGui import QPalette, QColor, QIcon, QPixmap, QPainter
+from PyQt6.QtCore import Qt, QDate, QSize
 import sys
 from connect import create_connection
 import os
 import datetime
+
+
+class ASSETS:
+    ICON = 'assets\logo5.png'
+    HELP_ICON = 'assets\help.png'
 
 
 class ProxyStyle(QProxyStyle):
@@ -17,10 +23,12 @@ class ProxyStyle(QProxyStyle):
     pass
 
 
-class table_model(QSqlRelationalTableModel):
+class TableModel(QSqlRelationalTableModel):
     def __init__(self):
-        super(table_model, self).__init__()
+        super(TableModel, self).__init__()
         self.pk_edit = False
+        self.setEditStrategy(
+            QSqlRelationalTableModel.EditStrategy.OnFieldChange)
 
     def set_pk_edit(self, on):
         self.pk_edit = on
@@ -34,10 +42,10 @@ class table_model(QSqlRelationalTableModel):
         return cur_flags
 
     def submit(self):
-        # print('Submitting')
+        print('Submitting')
         success = True
 
-        if self.editStrategy == QSqlRelationalTableModel.EditStrategy.OnFieldChange or self.editStrategy == QSqlRelationalTableModel.EditStrategy.OnRowChange:
+        if self.editStrategy() == QSqlRelationalTableModel.EditStrategy.OnFieldChange or self.editStrategy() == QSqlRelationalTableModel.EditStrategy.OnRowChange:
             res = super().submitAll()
             if res is False:
                 success = False
@@ -59,91 +67,151 @@ class table_model(QSqlRelationalTableModel):
             super().sort(column, self._sort_order)
 
 
-class table_widget(QWidget):
+class HelpDialogue(QWidget):
+    def __init__(self,):
+        super(HelpDialogue, self).__init__()
+        self.setFixedSize(500, 300)
+        self.setWindowTitle('Help')
+
+        self.set_icon(ASSETS.HELP_ICON)
+
+        self.layout = QVBoxLayout(self)
+
+        close_btn = QPushButton('Close')
+
+        close_btn.clicked.connect(self.close)
+
+        message = QLabel("Something happened, is that OK?")
+        self.layout.addWidget(message)
+        self.layout.addWidget(close_btn)
+        self.setLayout(self.layout)
+
+    def set_icon(self, relative_path):
+        self.setWindowIcon(QIcon(full_path(relative_path)))
+
+
+class PictureButton(QAbstractButton):
+
+    def __init__(self, picture, parent):
+        super().__init__(parent)
+        self.setPicture(QPixmap(picture))
+
+    def setPicture(self, picture):
+        self.picture = picture
+        self.update()
+
+    def sizeHint(self):
+        return self.picture.size()
+
+    def paintEvent(self, e):
+        painter = QPainter(self)
+        painter.drawPixmap(0, 0, self.picture)
+
+
+class MainWidget(QWidget):
     def __init__(self,  DATABASE_NAME, TABLE_NAME):
         self.DATABASE_NAME = DATABASE_NAME
         self.TABLE_NAME = TABLE_NAME
 
-        super(table_widget, self).__init__()
+        super(MainWidget, self).__init__()
 
         self.setFixedSize(700, 480)
         self.setWindowTitle(
             '{} @ Admin Panel'.format(self.TABLE_NAME.replace("dbo.", "")))
 
-        self.set_icon('assets\logo4.png')
+        # self.setWindowFlags(
+        #     Qt.WindowType.WindowContextHelpButtonHint and self.windowFlags())
+        self.set_icon(ASSETS.ICON)
 
-        self.layout = QVBoxLayout(self)
-
-        self.model = table_model()
+        self.model = TableModel()
         self.model.setTable(TABLE_NAME)
-
-        self.model.setEditStrategy(
-            QSqlRelationalTableModel.EditStrategy.OnFieldChange)
 
         self.view = QTableView()
         self.view.setModel(self.model)
         self.view.resizeColumnsToContents()
-        self.selection_model = self.view.selectionModel()
         self.view.setSelectionBehavior(
             QAbstractItemView.SelectionBehavior.SelectRows)
-        self.layout.addWidget(self.view)
-        # self.layout.addWidget(self.label)
-        # self.view.setFocusPolicy(Qt.FocusPolicy.NoFocus)
 
-        self.hh = self.view.horizontalHeader()
+        self.selection_model = self.view.selectionModel()
+
+        self.hh = self.setup_horizontal_header()
+        self.vh = self.setup_vertical_header()
+
+        self.view.setSortingEnabled(True)
+        self.hh.setSortIndicator(0, Qt.SortOrder.AscendingOrder)
+
+        self.help_widget = self.setup_help()
+        
+
+        self.setLayout(self.setup_layout())
+        self.model.select()
+
+    def setup_horizontal_header(self):
+        hh = self.view.horizontalHeader()
         for i in range(0, self.model.columnCount()):
-            self.hh.setSectionResizeMode(
+            hh.setSectionResizeMode(
                 i, QHeaderView.ResizeMode.ResizeToContents)
-        self.hh.setSectionResizeMode(3, QHeaderView.ResizeMode.Stretch)
-        self.hh.setSectionResizeMode(2, QHeaderView.ResizeMode.Fixed)
-        self.hh.setSectionResizeMode(4, QHeaderView.ResizeMode.Fixed)
+        hh.setSectionResizeMode(3, QHeaderView.ResizeMode.Stretch)
+        hh.setSectionResizeMode(2, QHeaderView.ResizeMode.Fixed)
+        hh.setSectionResizeMode(4, QHeaderView.ResizeMode.Fixed)
         self.view.setColumnWidth(4, 120)
         self.view.setColumnWidth(4, 70)
 
-        bold_font = self.hh.font()
+        bold_font = hh.font()
         bold_font.setBold(True)
-        self.hh.setFont(bold_font)
-        self.hh.setHighlightSections(True)
+        hh.setFont(bold_font)
+        hh.setHighlightSections(True)
+        return hh
 
-        self.vh = self.view.verticalHeader()
+    def setup_vertical_header(self):
+        vh = self.view.verticalHeader()
         # self.vh.setSectionResizeMode(QHeaderView.ResizeMode.Fixed)
         # self.vh.setFixedSize(20, 20)
-        self.vh.setMinimumWidth(25)
+        vh.setMinimumWidth(25)
         # self.vh.setAutoFillBackground(True)
-        self.vh.setStyleSheet(
+        vh.setStyleSheet(
             'QHeaderView {background-color: #f2f2f2;}')
+        return vh
 
-        self.view.setSortingEnabled(True)
-
-        # self._style = ProxyStyle(self.view.style())
-        # self.view.setStyle(self._style)
-        # self.model.setProperty('hideSortIndicatorColumn', 3)
-        # self.model.sort(0, Qt.SortOrder.DescendingOrder)
-        # self.model.sort(0, Qt.SortOrder.AscendingOrder)
-        # self.hh.setSortOrder(Qt.SortOrder.AscendingOrder)
-        self.hh.setSortIndicator(0, Qt.SortOrder.AscendingOrder)
-        cur_sort_order = self.view.horizontalHeader().sortIndicatorOrder()
-        print(cur_sort_order)
-
+    def setup_layout(self):
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.view)
         button_box = QWidget()
         button_box.layout = QHBoxLayout(button_box)
 
+        help_btn = QPushButton()
+
+        pixmap = QPixmap(full_path(ASSETS.HELP_ICON))
+        help_btn.setIcon(QIcon(pixmap))
+        help_btn_size = QSize(25, 25)
+        help_btn.setIconSize(help_btn_size)
+        help_btn.setFixedSize(help_btn_size)
+        help_btn.setStyleSheet(
+            "QPushButton { background-color: transparent; border: 0px }")
+
         insert_btn = QPushButton('Insert Record')
         delete_btn = QPushButton('Delete Record')
+        help_btn.clicked.connect(self.show_help)
         insert_btn.clicked.connect(self.insert_record)
         delete_btn.clicked.connect(self.delete_records)
 
+        button_box.layout.addWidget(help_btn)
         button_box.layout.addSpacing(300)
         button_box.layout.addWidget(insert_btn)
         button_box.layout.addWidget(delete_btn)
 
-        self.layout.addWidget(button_box)
+        layout.addWidget(button_box)
+        return layout
 
-        self.model.select()
+    def setup_help(self):
+        print('Displaying help dialogue')
+
+        # help_dialogue.show()
+        help_dialogue =  HelpDialogue()
+        return help_dialogue
 
     def set_icon(self, relative_path):
-        scriptDir = os.path.dirname(os.path.realpath(__file__))
-        self.setWindowIcon(QIcon(scriptDir + os.path.sep + relative_path))
+        self.setWindowIcon(QIcon(full_path(relative_path)))
 
     def submit_changes(self):
         print('Submitting changes')
@@ -202,19 +270,14 @@ class table_widget(QWidget):
             print('Delete failed')
         self.model.select()
 
+    def show_help(self):
+        print('Showing help')
+        self.help_widget.show()
 
-def display_data(sqlStatement):
-    print('Processing query')
-    qry = QSqlQuery(db)
-    qry.prepare(sqlStatement)
-    qry.exec()
 
-    model = QSqlQueryModel()
-    model.setQuery(qry)
-
-    widget = QTableView()
-    widget.setModel(model)
-    return widget
+def full_path(relative_path):
+    scriptDir = os.path.dirname(os.path.realpath(__file__))
+    return scriptDir + os.path.sep + relative_path
 
 
 def set_style(app):
@@ -222,14 +285,15 @@ def set_style(app):
     # app.setStyle("Fusion")
 
     palette = QPalette()
-    palette.setColor(QPalette.ColorRole.Window, QColor(231, 231, 231))
+    palette.setColor(QPalette.ColorRole.Window, QColor(220, 220, 220))
     # palette.setColor(QPalette.ColorRole.WindowText, Qt.GlobalColor.white)
-    # palette.setColor(QPalette.ColorRole.Base, QColor(25, 25, 25))
+    palette.setColor(QPalette.ColorRole.Base, QColor(242, 242, 242))
     # palette.setColor(QPalette.ColorRole.AlternateBase, QColor(53, 53, 53))
     # palette.setColor(QPalette.ColorRole.ToolTipBase, Qt.GlobalColor.white)
     # palette.setColor(QPalette.ColorRole.ToolTipText, Qt.GlobalColor.white)
     # palette.setColor(QPalette.ColorRole.Text, Qt.GlobalColor.white)
-    palette.setColor(QPalette.ColorRole.Button, QColor(190, 232, 190))
+    palette.setColor(QPalette.ColorRole.Button, QColor(70, 152, 27))
+    # palette.setColor(QPalette.ColorRole.Button, QColor(147, 223, 147))
     palette.setColor(QPalette.ColorRole.ButtonText, Qt.GlobalColor.black)
     # palette.setColor(QPalette.ColorRole.BrightText, Qt.GlobalColor.red)
     # palette.setColor(QPalette.ColorRole.Link, QColor(42, 130, 218))
@@ -248,7 +312,7 @@ def start(DRIVER_NAME, SERVER_NAME, DATABASE_NAME, TABLE_NAME):
     global db
     from connect import db
 
-    main_window = table_widget(DATABASE_NAME, TABLE_NAME)
+    main_window = MainWidget(DATABASE_NAME, TABLE_NAME)
     main_window.show()
 
     sys.exit(app.exec())
