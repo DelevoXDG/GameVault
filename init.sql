06b6600c974ab3ab9dbba831137e9cf51a466677
Delevo <43750648+DelevoXDG@users.noreply.github.com>
2023-02-23 06:24:03 +0100

    Dev (#3)
    Added some procs and funcs

--- init.sql
+++ init.sql
@@ -1,4 +1,5 @@
 
+
 IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'Steam')
 BEGIN
   CREATE DATABASE Steam
@@ -325,7 +326,7 @@ CREATE TABLE Orders (
   OrderId INT PRIMARY KEY IDENTITY(1,1),
   UserId INT NOT NULL,
   OrderDate DATE NOT NULL,
-  [Total amount in USD] MONEY NOT NULL CHECK ([Total amount in USD] >= 0),
+
   FOREIGN KEY (UserId) REFERENCES Users (UserId) ON DELETE CASCADE ON UPDATE CASCADE
 );
 GO
@@ -361,7 +362,8 @@ VALUES
   ('jane_doe', 'jane_doe@example.com', 'password2'),
   ('jim_smith', 'jim_smith@example.com', 'password3'),
   ('sara_lee', 'sara_lee@example.com', 'password4'),
-  ('tom_jones', 'tom_jones@example.com', 'password5');
+  ('tom_jones', 'tom_jones@example.com', 'password5'),
+  ('jimmy_johns', 'big_jimmy@example.com', 'password6');
   
 INSERT INTO Games (Title, LastUpdatedDate, Description, [Price in USD])
 VALUES
@@ -369,7 +371,8 @@ VALUES
   ('Red Dead Redemption 2', '2022-09-25', 'Live the life of an outlaw in a stunning open world filled with memorable characters and tough choices.', 59.99),
   ('God of War', '2022-10-01', 'Journey with Kratos and his son Atreus through Norse mythology in this epic adventure.', 39.99),
   ('Halo 5: Guardians', '2022-06-15', 'Join Master Chief and Spartan Locke in a battle to save the galaxy from a new threat.', 59.99),
-  ('Minecraft', '2022-11-30', 'Unleash your creativity and build anything you can imagine in a blocky, procedurally generated world.', 26.95);
+  ('Minecraft', '2022-11-30', 'Unleash your creativity and build anything you can imagine in a blocky, procedurally generated world.', 26.95),
+  ('Cyberpunk 2077', '2022-01-15', 'Experience the gritty world of Night City in this action-packed RPG.', 49.99);
 
 INSERT INTO GameGenres (GameId, Genre)
 VALUES
@@ -495,21 +498,39 @@ VALUES
   (4, 5, 4),
   (5, 1, 5);
 
-INSERT INTO Orders (UserId, OrderDate, [Total amount in USD]) 
+INSERT INTO Orders (UserId, OrderDate) 
 VALUES 
-  (1, '2022-12-01', 200.00),
-  (2, '2022-11-15', 150.00),
-  (3, '2022-10-31', 100.00),
-  (4, '2022-09-15', 75.00),
-  (5, '2022-08-01', 50.00);
+  (1, '2023-01-07'),
+  (2, '2022-12-30'),
+  (3, '2022-12-03'),
+  (4, '2022-09-15'),
+  (5, '2022-08-01'),
+  (6, '2022-07-15');
 
 INSERT INTO OrderItems (OrderId, GameId, Quantity, [Price in USD]) 
 VALUES 
-  (1, 1, 2, 59.99),
-  (2, 2, 1, 49.99),
-  (3, 3, 3, 39.99),
-  (4, 4, 4, 29.99),
-  (5, 5, 5, 19.99);
+  (1, 1, 2, 29.99),
+  (1, 2, 1, 9.99),
+  (1, 3, 3, 44.97),
+  (1, 4, 1, 19.99),
+  (1, 5, 2, 29.98),
+  (2, 2, 1, 9.99),
+  (2, 3, 2, 29.98),
+  (2, 5, 1, 14.99),
+  (3, 1, 1, 14.99),
+  (3, 2, 1, 9.99),
+  (3, 3, 1, 14.99),
+  (3, 4, 2, 39.98),
+  (3, 5, 1, 14.99),
+  (4, 1, 1, 14.99),
+  (4, 3, 1, 14.99),
+  (4, 4, 1, 19.99),
+  (4, 5, 1, 14.99),
+  (5, 1, 2, 29.98),
+  (5, 2, 1, 9.99),
+  (5, 4, 2, 39.98),
+  (5, 6, 1, 49.99);
+  
 
 INSERT INTO ExchangeRate (Currency, [Equal 1 USD]) 
 VALUES 
@@ -519,6 +540,7 @@ VALUES
   ('JPY', 134.15),
   ('PLN', 4.45);
 GO
+
 
 
 IF OBJECT_ID('HowMuch', 'FN') IS NOT NULL
@@ -591,20 +613,180 @@ BEGIN
 END;
 GO
 
-IF OBJECT_ID('dbo.GetTopRatedGames', 'P') IS NOT NULL
-  DROP PROCEDURE dbo.GetTopRatedGames
+IF OBJECT_ID('TopRatedGames', 'V') IS NOT NULL
+  DROP VIEW TopRatedGames
 GO
-CREATE PROCEDURE GetTopRatedGames (@TOP_COUNT INTEGER) 
+
+CREATE VIEW TopRatedGames AS
+  SELECT TOP 100 PERCENT G.GameID, G.Title, AVG(S.Score) AS [Average Score], COUNT(R.GameId) AS [Number of Reviews]
+  FROM Games AS G
+  LEFT JOIN Score AS S 
+  ON G.GameID = S.GameID
+  LEFT JOIN Reviews AS R
+  ON G.GameID = R.GameID
+  GROUP BY 
+    G.GameId, 
+    G.Title
+  ORDER BY 
+    [Average Score] DESC,
+    [Number of Reviews] DESC; 
+GO
+
+
+
+IF OBJECT_ID('GetRecommendedGames') IS NOT NULL
+  DROP PROCEDURE GetRecommendedGames
+GO
+
+  CREATE PROCEDURE GetRecommendedGames (@UserId INT)
+  AS
+  BEGIN
+  WITH UserOrders AS (
+    SELECT OrderId
+    FROM Orders
+    WHERE UserId = @UserId
+  ),
+  UserGames AS (
+    SELECT DISTINCT GameId
+    FROM OrderItems
+    WHERE OrderId IN (SELECT OrderId FROM UserOrders)
+  ),
+  MatchingUsers AS (
+    SELECT 
+      UserId, 
+      COUNT(DISTINCT GameId) AS [Matching Games Num]
+    FROM OrderItems AS OI
+    JOIN Orders AS O
+    ON OI.OrderId = O.OrderId
+    WHERE 
+      GameId IN (SELECT GameId FROM UserGames)
+      AND UserId <> @UserId
+    GROUP BY UserId
+  )
+  , MatchingGames AS (
+  SELECT 
+      G.GameID,
+      G.Title,
+      U.[Matching Games Num] ,
+      ROW_NUMBER() OVER (PARTITION BY G.Title ORDER BY U.[Matching Games Num] DESC) AS RowNum
+    FROM MatchingUsers U
+    JOIN Orders AS O ON U.UserId = O.UserId
+    JOIN OrderItems AS OI ON O.OrderId = OI.OrderId
+    JOIN Games AS G ON OI.GameId = G.GameId
+    WHERE U.[Matching Games Num] >= 1
+    GROUP BY G.GameId, G.Title, U.[Matching Games Num], U.UserId
+)
+SELECT GameId, Title
+FROM MatchingGames
+WHERE RowNum = 1
+ORDER BY [Matching Games Num] DESC
+END;
+GO
+
+-- EXEC GetRecommendedGames 4
+IF OBJECT_ID('CalculateTotalSales') IS NOT NULL
+  DROP PROCEDURE CalculateTotalSales
+GO
+
+CREATE PROCEDURE CalculateTotalSales 
+  @StartDate DATE = NULL, 
+  @EndDate DATE = NULL, 
+  @GameId INT = NULL
 AS
 BEGIN
-  SELECT TOP (@TOP_COUNT) g.Title, AVG(S.Score) AS AverageRating
-  FROM Games G
-  JOIN Score S 
-  ON g.GameID = S.GameID
-  GROUP BY G.Title
-  ORDER BY AverageRating DESC
-END
+  SELECT 
+    G.Title AS GameTitle, 
+    SUM(OI.Quantity * OI.[Price in USD]) AS TotalSales
+  FROM 
+    OrderItems OI 
+    LEFT JOIN Games G ON OI.GameId = G.GameId
+    LEFT JOIN Orders O ON OI.OrderId = O.OrderId
+  WHERE 
+    (@StartDate IS NULL OR O.OrderDate >= @StartDate)
+    AND (@EndDate IS NULL OR O.OrderDate <= @EndDate)
+    AND (@GameId IS NULL OR G.GameId = @GameId)
+  GROUP BY 
+    G.Title
+  ORDER BY 
+    TotalSales DESC;
+END;
 GO
 -- EXEC GetTopRatedGames 10
+-- GO
+
+-- EXEC CalculateTotalSales 
+-- GO
+IF OBJECT_ID('CalculateTotalRevenue', 'FN') IS NOT NULL
+  DROP FUNCTION CalculateTotalRevenue
+GO
+
+CREATE FUNCTION CalculateTotalRevenue ()
+RETURNS MONEY
+AS
+BEGIN
+  DECLARE @TotalRevenue MONEY
+  SELECT @TotalRevenue = SUM(OI.Quantity * OI.[Price in USD])
+  FROM OrderItems OI
+  LEFT JOIN Games G ON OI.GameId = G.GameId
+  LEFT JOIN Orders O ON OI.OrderId = O.OrderId
+  WHERE O.OrderDate IS NOT NULL
+  
+  RETURN @TotalRevenue
+END;
+GO
+-- SELECT dbo.CalculateTotalRevenue()
+-- GO
+
+IF OBJECT_ID('SearchUsers') IS NOT NULL
+  DROP PROCEDURE SearchUsers
+GO
+CREATE PROCEDURE SearchUsers
+  @Username VARCHAR(255)
+AS
+  SELECT UserID, Username 
+  FROM Users
+  WHERE Username LIKE '%'+@Username+'%'
+GO
+-- EXEC SearchUsers 'jo'
+-- GO
+IF OBJECT_ID('GetMostActiveUsers', 'P') IS NOT NULL
+  DROP PROCEDURE GetMostActiveUsers
+GO
+CREATE PROCEDURE GetMostActiveUsers
+  @StartDate DATE = NULL,
+  @EndDate DATE = NULL,
+  @OrderBy VARCHAR(20) = 'TotalSpent' -- Default to sorting by TotalSpent in descending order
+AS
+BEGIN
+  WITH UserPurchaseInfo AS (
+    SELECT
+      O.UserId,
+      COUNT(DISTINCT OI.GameID) AS GamesBoughtNum,
+      SUM(OI.[Price in USD] * OI.Quantity) AS TotalSpent
+    FROM
+      Orders O
+      JOIN OrderItems OI ON O.OrderId = OI.OrderId
+    WHERE
+      (@StartDate IS NULL OR O.OrderDate >= @StartDate)
+      AND (@EndDate IS NULL OR O.OrderDate <= @EndDate)
+    GROUP BY
+      O.UserId
+  )
+  SELECT
+    U.UserId,
+    PI.GamesBoughtNum,
+    PI.TotalSpent
+  FROM
+    UserPurchaseInfo PI
+    JOIN Users U ON PI.UserId = U.UserId
+  ORDER BY
+    CASE
+      WHEN @OrderBy = 'TotalSpent' THEN PI.TotalSpent
+      WHEN @OrderBy = 'GamesBought' THEN PI.GamesBoughtNum
+    END DESC;
+END;
+GO
+
+-- EXEC GetMostActiveUsers '2022-12-01', '2022-12-31', 'GamesBought';
 -- GO
 
